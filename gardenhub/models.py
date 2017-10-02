a2a35f3935c4fba8f37500f22dabc8f8370e274b
Mary Kate Fain <mkfain1@gmail.com>
2017-10-01 22:15:47 -0400

    updated models and requirements

--- gardenhub/models.py
+++ gardenhub/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Crop(models.Model):
     title = models.CharField(max_length=255)
+    image = models.ImageField()
 
     def __str__(self):
         return self.title
@@ -11,7 +12,9 @@ class Crop(models.Model):
 
 class Garden(models.Model):
     title = models.CharField(max_length=255)
-    owners = models.ManyToManyField(User)
+    managers = models.ManyToManyField(User, related_name='+')
+    address = models.CharField(max_length=255)
+    gardeners = models.ManyToManyField(User, related_name='+')
 
     def __str__(self):
         return self.title
@@ -20,14 +23,38 @@ class Garden(models.Model):
 class Plot(models.Model):
     title = models.CharField(max_length=255)
     garden = models.ForeignKey('Garden')
+    gardener = models.ManyToManyField(User)
+    crops = models.ManyToManyField('Crop')
 
     def __str__(self):
         return self.title
 
 
-class Harvest(models.Model):
+class Order(models.Models):
+
     plot = models.ForeignKey('Plot')
     crops = models.ManyToManyField('Crop')
+    start_date = models.DateField()
+    end_date = models.DateField()
+    canceled = models.BooleanField
+    canceled_date = models.DateField(null=True, blank=True)
+    requester = models.ForeignKey(User)
+
+        def __str__(self):
+            return self.id
+
+
+class Harvest(models.Model):
+
+    harvest_date = models.DateField()
+    status = models.CharField(
+        max_length=255,
+        choices=(
+            ('incomplete', 'Incomplete'),
+            ('finished', 'Finished'),
+        ),
+    )
+    employee = models.ForeignKey(User)
 
     def __str__(self):
         return self.id
