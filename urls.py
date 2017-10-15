529d18b12be3ba914875c662cb3426917fdad6ab
Mary Kate Fain <mkfain1@gmail.com>
2017-10-15 17:33:17 -0400

    controllers and views updated

--- urls.py
+++ urls.py
@@ -17,13 +17,36 @@ from django.conf.urls import url, include
 from gardenhub import views
 from django.contrib import admin
 
+settings_patterns = [
+    url(r'^$', views.my_account),
+    url(r'^settings/', views.account_settings),
+    url(r'^delete/', views.delete_account),
+]
+
+harvest_patterns = [
+    url(r'^$', views.harvest),
+    url(r'^schedule/', views.schedule_harvest),
+    url(r'^upcoming/', views.upcoming_harvests),
+    url(r'^assignments/', views.harvest_assignments),
+    url(r'^record/', views.record_harvest),
+]
+
+manage_patterns = [
+    url(r'^$', views.manage),
+    url(r'^gardens/(?P<gardenId>.*)', views.manage_garden),
+    url(r'^gardens/(?P<gardenId>.*)/gardeners', views.manage_garden_gardeners),
+    url(r'^gardens/(?P<gardenId>.*)/gardeners/edit', views.manage_garden_gardeners_edit),
+    url(r'^gardens/(?P<gardenId>.*)/harvests', views.manage_garden_harvests),
+    url(r'^gardens/(?P<gardenId>.*)/settings', views.manage_garden_settings),
+]
 
 urlpatterns = [
     url(r'^$', views.login),
     url(r'^admin/', admin.site.urls),
     url(r'^home/', views.home),
-    url(r'^account/', views.account),
-    url(r'^plot/', views.plot),
-    url(r'^harvest/', views.harvest),
-    url(r'^manage/', views.manage),
+    url(r'^account/', include(settings_patterns)),
+    url(r'^edit_plot/', views.edit_plot),
+    url(r'^my_plots/', views.my_plots),
+    url(r'^harvest/', include(harvest_patterns)),
+    url(r'^manage/', include(manage_patterns)),
 ]
