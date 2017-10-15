4170b88e40b777601434efc80aeab94caa38fc52
Mary Kate Fain <mkfain1@gmail.com>
2017-10-15 14:55:18 -0400

    rearanged files, urls, and views to be task-based vs user-based

--- gardenhub/views.py
+++ gardenhub/views.py
@@ -9,17 +9,7 @@ from .models import Crop, Garden, Plot, Harvest
 
 
 def login(request):
-    return render(request, 'gardenhub/index.html', {
-        'foo': 'bar',
-    })
-
-def ep_garden_assignments(request):
-    return render(request, 'gardenhub/employee_views/ep_garden_assignments.html', {
-        'foo': 'bar',
-    })
-
-def ep_garden_map(request):
-    return render(request, 'gardenhub/employee_views/ep_garden_map.html', {
+    return render(request, 'gardenhub/login_templates/login.html', {
         'foo': 'bar',
     })
 
@@ -27,39 +17,43 @@ def home(request):
     user_group = 'gardener'
 
     if user_group == 'employee':
-        return render(request, 'gardenhub/employee_views/home.html')
+        return render(request, 'gardenhub/homescreen_templates/employee_home.html')
     elif user_group == 'gardener':
-        return render(request, 'gardenhub/gardener_views/home.html')
+        return render(request, 'gardenhub/homescreen_templates/gardener_home.html')
     elif user_group == 'manager':
-        return render(request, 'gardenhub/manager_views/home.html')
+        return render(request, 'gardenhub/homescreen_templates/manager_home.html')
 
-
-def edit_plot(request):
-    return render(request, 'gardenhub/gardener_views/edit_plot.html', {
+def account(request):
+    return render(request, 'gardenhub/account_settings_templates/my_account.html', {
         'foo': 'bar',
     })
 
-def my_gardens(request):
-    return render(request, 'gardenhub/gardener_views/my_gardens.html', {
+def plot(request):
+    return render(request, 'gardenhub/edit_plot_templates/edit_plot.html', {
         'foo': 'bar',
     })
 
-def schedule(request):
-    return render(request, 'gardenhub/gardener_views/schedule.html', {
+def plot(request):
+    return render(request, 'gardenhub/manage_gardens_templates/my_gardens.html', {
         'foo': 'bar',
     })
 
-def manage_garden(request):
-    return render(request, 'gardenhub/manager_views/manage_garden.html', {
-        'foo': 'bar',
-    })
+def harvest(request):
+    user_group = 'gardener'
 
-def manage_gardens_select(request):
-    return render(request, 'gardenhub/manager_views/manage_gardens_select.html', {
-        'foo': 'bar',
-    })
+    if user_group == 'employee':
+        return render(request, 'gardenhub/harvest_templates/harvest_assignments_templates/_templates/ep_garden_assignments.html')
+    elif user_group == 'gardener':
+        return render(request, 'gardenhub/harvest_templates/schedule_harvest_templates/schedule.html')
+    elif user_group == 'manager':
+        return render(request, 'gardenhub/harvest_templates/upcoming_harvest_templates/upcoming_harvests.html')
 
-def view_gardeners(request):
-    return render(request, 'gardenhub/manager_views/view_gardeners.html', {
-        'foo': 'bar',
-    })
+def manage(request):
+    user_group = 'manager'
+
+    if user_group == 'employee':
+        return render(request, 'gardenhub/homescreen_templates/employee_home.html')
+    elif user_group == 'gardener':
+        return render(request, 'gardenhub/homescreen_templates/gardener_home.html')
+    elif user_group == 'manager':
+        return render(request, 'gardenhub/manage_gardens_templates/my_gardens.html')
