41f5860abad887a46f2a92386ec5d58e5b4add5d
Alex Gleason <alex@alexgleason.me>
2017-11-05 17:53:16 -0500

    Login screen refactor

--- static/main.js
+++ static/main.js
@@ -1,7 +1,4 @@
 function nextLogin() {
-  document.getElementById('next').classList.toggle('hide');
-  document.getElementById('login').classList.toggle('hide');
-  document.getElementById('email_field').classList.toggle('hide');
-  document.getElementById('password_field').classList.toggle('hide');
+  document.querySelector('.login_page form').classList.toggle('password');
   document.getElementById('directions').innerHTML = "Please enter your password";
 }
