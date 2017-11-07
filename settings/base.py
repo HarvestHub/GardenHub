669db4f8140bc3da156f0a4fcc948f3491dfd41c
Alex Gleason <alex@alexgleason.me>
2017-11-07 18:52:21 -0500

    Fix static files for prod

--- settings/base.py
+++ settings/base.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_prod')
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
