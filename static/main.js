8397eb99fff284b521751c9bef2ed104509c71c0
Mary Kate Fain <mkfain1@gmail.com>
2017-11-09 20:56:29 -0500

    manage nav dropdown

--- static/main.js
+++ static/main.js
@@ -16,4 +16,10 @@ document.querySelector('#email_field input').focus();
 
 function toggleUserOptionsNav() {
   document.getElementById('user_options').classList.toggle('hidden');
+  document.getElementById('manage_options').classList.add('hidden');
+};
+
+function toggleMangeOptionsNav() {
+  document.getElementById('manage_options').classList.toggle('hidden');
+  document.getElementById('user_options').classList.add('hidden');
 };
