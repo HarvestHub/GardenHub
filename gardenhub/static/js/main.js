ec839a2f0fd42c6f2bac7a9676c355e24d1bec2e
Mary Kate Fain <mkfain1@gmail.com>
2017-11-16 11:24:23 -0500

    typos in mainjs

--- gardenhub/static/js/main.js
+++ gardenhub/static/js/main.js
@@ -1,12 +1,12 @@
 function toggleUserOptionsNav() {
   document.getElementById('user_options').classList.toggle('hidden');
   document.getElementById('manage_options').classList.add('hidden');
-};
+}
 
 function toggleMangeOptionsNav() {
   document.getElementById('manage_options').classList.toggle('hidden');
   document.getElementById('user_options').classList.add('hidden');
-};
+}
 
 // Select crops to harvest
 
