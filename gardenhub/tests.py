a97d44d8809b29181581418c128e111173a842d1
Alex Gleason <alex@alexgleason.me>
2017-12-06 22:37:51 -0500

    Add more User tests

--- gardenhub/tests.py
+++ gardenhub/tests.py
@@ -1,12 +1,29 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model, authenticate
-from .models import Garden, Plot
+from .models import Garden, Plot, Order
 
 
 class UserTestCase(TestCase):
     """
     Test User model methods.
     """
+
+    def setUp(self):
+        # A garden and plot are first needed
+        garden = Garden.objects.create(title='Garden A', address='1000 Garden Rd, Philadelphia PA, 1776')
+        plot = Plot.objects.create(title='Plot 1', garden=garden)
+
+        # Create a gardener of a single plot
+        self.gardener = get_user_model().objects.create_user(email='gardener@gardenhub.dev', password='gardener')
+        plot.gardeners.add(self.gardener)
+
+        # Create a garden manager of a single garden and no plots
+        self.garden_manager = get_user_model().objects.create_user(email='garden_manager@gardenhub.dev', password='garden_manager')
+        garden.managers.add(self.garden_manager)
+
+        # Create a normal user who isn't assigned to anything
+        self.normal_user = get_user_model().objects.create_user(email='normal_user@gardenhub.dev', password='normal_user')
+
 
     def test_create_user(self):
         """ Create a user """
@@ -90,3 +107,85 @@ class UserTestCase(TestCase):
         # Test results
         result = [plots[0], plots[1], plots[2]]
         self.assertEqual(list(user.get_plots()), result)
+
+
+    def test_get_orders(self):
+        """ User.get_orders() """
+        # TODO: Writing a test for this will be painful and I don't feel like it right now
+        pass
+
+
+    def test_is_garden_manager(self):
+        """ User.is_garden_manager() """
+
+        # Test a garden manager
+        self.assertTrue(self.garden_manager.is_garden_manager())
+
+        # Test *not* garden managers
+        self.assertFalse(self.gardener.is_garden_manager())
+        self.assertFalse(self.normal_user.is_garden_manager())
+
+
+    def test_is_gardener(self):
+        """ User.is_gardener() """
+
+        # Test a gardener of a single plot
+        self.assertTrue(self.gardener.is_gardener())
+
+        # Test that a garden manager is also considered a gardener
+        self.assertTrue(self.garden_manager.is_gardener())
+
+        # Create and test a *not* gardener
+        self.assertFalse(self.normal_user.is_gardener())
+
+
+    def test_is_anything(self):
+        """ User.is_anything() """
+
+        # Test a normal user
+        self.assertFalse(self.normal_user.is_anything())
+
+        # Test a gardener and garden manager
+        self.assertTrue(self.gardener.is_anything())
+        self.assertTrue(self.garden_manager.is_anything())
+
+
+    def test_has_open_orders(self):
+        """ User.has_open_orders() """
+        # TODO
+        pass
+
+
+    def test_can_edit_garden(self):
+        """ User.can_edit_garden() """
+
+        # Create garden
+        garden = Garden.objects.create(title='Garden A', address='1000 Garden Rd, Philadelphia PA, 1776')
+
+        # Assign garden manager to garden
+        garden_manager = get_user_model().objects.create_user(email='garden_manager2@gardenhub.dev', password='garden_manager2')
+        garden.managers.add(garden_manager)
+
+        # Test that the GM can edit the garden
+        self.assertTrue(garden_manager.can_edit_garden(garden))
+
+        # Test that a normal user can't edit the garden
+        self.assertFalse(self.normal_user.can_edit_garden(garden))
+
+
+    def test_can_edit_plot(self):
+        """ User.can_edit_plot() """
+
+        # Create plot
+        garden = Garden.objects.create(title='Garden A', address='1000 Garden Rd, Philadelphia PA, 1776')
+        plot = Plot.objects.create(title='Plot 1', garden=garden)
+
+        # Assign gardener to plot
+        gardener = get_user_model().objects.create_user(email='gardener2@gardenhub.dev', password='gardener2')
+        plot.gardeners.add(gardener)
+
+        # Test that the gardener can edit the plot
+        self.assertTrue(gardener.can_edit_plot(plot))
+
+        # Test that a normal user can't edit the garden
+        self.assertFalse(self.normal_user.can_edit_plot(plot))
