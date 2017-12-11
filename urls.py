0e99e954163924579e56c76603abefa4e54f595f
Alex Gleason <alex@alexgleason.me>
2017-12-11 18:24:54 -0500

    Switch to Django 2.0 path() for urls

--- urls.py
+++ urls.py
@@ -5,47 +5,48 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('$', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url, include
+from django.urls import path
+from django.conf.urls import include
 from django.conf.urls.static import static
 from gardenhub import views
 from django.contrib import admin
 
 
 account_patterns = [
-    url(r'^$', views.my_account),
-    url(r'^settings/', views.account_settings),
-    url(r'^delete/', views.delete_account),
+    path('', views.my_account),
+    path('settings/', views.account_settings),
+    path('delete/', views.delete_account),
 ]
 
 urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^login/$', views.login_user),
-    url(r'^logout/$', views.logout_user),
-    url(r'^admin/', admin.site.urls),
-    url(r'^account/', include(account_patterns)),
-    url(r'^activate/(?P<uuid>[0-9a-z\-]+)/$', views.activate_account),
+    path('', views.home),
+    path('login/', views.login_user),
+    path('logout/', views.logout_user),
+    path('admin/', admin.site.urls),
+    path('account/', include(account_patterns)),
+    path('activate/<uuid:uuid>/', views.activate_account),
     # Orders
-    url(r'^orders/$', views.orders),
-    url(r'^orders/new/$', views.new_order),
-    url(r'^order/(?P<orderId>[0-9]+)/$', views.view_order),
+    path('orders/', views.orders),
+    path('orders/new/', views.new_order),
+    path('order/<int:orderId>/', views.view_order),
     # Plots
-    url(r'^plots/$', views.plots),
-    url(r'^plot/(?P<plotId>[0-9]+)/edit/$', views.edit_plot),
+    path('plots/', views.plots),
+    path('plot/<int:plotId>/edit/', views.edit_plot),
     # Gardens
-    url(r'^gardens/$', views.gardens),
-    url(r'^garden/(?P<gardenId>[0-9]+)/$', views.view_garden),
-    url(r'^garden/(?P<gardenId>[0-9]+)/edit/$', views.edit_garden),
+    path('gardens/', views.gardens),
+    path('garden/<int:gardenId>/', views.view_garden),
+    path('garden/<int:gardenId>/edit/', views.edit_garden),
     # API
-    url(r'^_api/crops/(?P<plotId>[0-9]+)/$', views.api_crops),
+    path('_api/crops/<int:plotId>/', views.api_crops),
     # Default auth views https://docs.djangoproject.com/en/2.0/topics/auth/default/#using-the-views
-    url('^', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
