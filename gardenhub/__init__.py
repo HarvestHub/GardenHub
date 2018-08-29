8f3aba835174db56eb891605b48767bcdc8d40cb
Alex Gleason <alex@alexgleason.me>
2018-08-29 19:09:56 -0400

    Bump version 1.1.0

--- gardenhub/__init__.py
+++ gardenhub/__init__.py
@@ -2,6 +2,6 @@ from django.utils.version import get_version
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 0, 0, 'final', 0)
+VERSION = (1, 1, 0, 'final', 0)
 
 __version__ = get_version(VERSION)
